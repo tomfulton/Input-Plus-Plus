04a52689e67bb382a0eff494d1da3ba87ca5a49e
Ryan McDonough <ryan.mcdonough@epiphanysolutions.co.uk>
2015-11-26 11:32:01 +0000

    Nuget av.

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 
 REM Restore dependencies
 call npm install
-app\Epiphany.InputPlusPlus\.nuget\nuget.exe restore app\Epiphany.InputPlusPlus\Epiphany.InputPlusPlus.sln
+nuget restore app\Epiphany.InputPlusPlus\Epiphany.InputPlusPlus.sln
 
 REM Build the nuget package
 call grunt nuget
